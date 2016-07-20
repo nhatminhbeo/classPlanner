4e4aead9ce32511ed88bf1ea0bb258c674cbc122
Minh Tran <tqnmb437@gmail.com>
2016-07-20 09:59:45 +0000

    Done Description 1

---
+++ sources/getAll2.py
@@ -0,0 +1,35 @@
+import lxml
+from lxml import html
+import requests
+import json
+import sys
+import re
+from bs4 import BeautifulSoup
+
+
+f = open('final_res2', 'r+')
+
+with open('url2') as urlfile:
+	for line in urlfile:
+		url = "http://ucsd.edu/catalog/courses/"+line
+		url = url.strip()
+		#print(url)
+		#print('\n')
+
+		page = requests.get(url)
+		soup = BeautifulSoup(page.content, 'lxml')
+		for cnamee in soup.find_all("p", class_='course-name'):
+			cname = cnamee.text
+                        cname = cname.strip(' \t\n\r ')
+                        cname = re.sub('[\t\n\r]', '', cname)
+                        while "  " in cname:
+                                cname = re.sub('  ', ' ', cname)
+			f.write(cname.encode("utf-8"))
+			f.write("\n")
+
+			cdepe = cnamee.find_next("p")
+			f.write(cdepe.text.encode("utf-8"))
+			f.write("\n\n")
+			
+f.close()
+
--- sources/getAllClass.py
+++ sources/getAllClass.py
@@ -6,6 +6,18 @@ import sys
 import re
 
 
+def edgeCase(cname, cdep, item):
+
+	if "ANSC 100" in cname[item]:
+		cdep.insert(item, "Course usually taught by visiting faculty in sociocultural anthropology. Course will vary in title and content. When offered, the current description and title is found in the current Schedule of Classes and the anthropology department website. (Can be taken a total of four times as topics vary.) Prerequisites: upper-division standing or consent of instructor. (Formerly known as ANGN 100.)")
+
+	if "ANSC 164" in cname[item]:
+		cdep.insert(item, "Basic concepts and theory of medical anthropology are introduced and applied to comparison of medical systems including indigenous and biomedical, taking into account cross-cultural variation in causal explanation, diagnosis, perception, management, and treatment of illness and disease. Prerequisites: upper-division standing.")
+
+	if "ANTH 201" in cname[item]:
+		cdep.insert(item, "Course usually taught by visiting faculty in anthropological archaeology. Course will vary in title and content. When offered, the current description and title is found in the current Schedule of Classes on TritonLink, and the Department of Anthropology website. (Can be taken a total of four times as topics vary.) (Formerly known as ANGR 201.)")
+
+
 f = open('final_res', 'r+')
 
 with open('url2') as urlfile:
@@ -27,7 +39,7 @@ with open('url2') as urlfile:
 		cname = tree.xpath( "//p[@class=\"course-name\"]/text() | //p[@class=\"course-name\"]/descendant::*/text()" )
 		#cdep = tree.xpath( "//p[@class=\"course-descriptions\"]/text()" )
 		pattern = r'course-descriptions\">(.*?)<\/p'
-		cdep = re.findall(pattern, page.content)
+		cdep = re.findall(pattern, page.content, re.I|re.S)
 		#print(cdep)
 		#print cname
 		#print cdep[:30]
@@ -35,16 +47,22 @@ with open('url2') as urlfile:
 		cdep2 = []
 
 		#print cname
+		item2 = -1
 		for item in range(len(cname)):
+			item2 = item2 + 1
+			if ((page.content.decode("utf-8").find(cname[item])) < (page.content.find(cdep[item2]))):
+				cdep.insert(item2, "")
 			cname[item] = cname[item].strip(' \t\n\r ')
 			cname[item] = re.sub('[\t\n\r]', '', cname[item])
 			while "  " in cname[item]:
 				cname[item] = re.sub('  ', ' ', cname[item])
+			#edgeCase(cname, cdep, item)
 			f.write (cname[item].encode("utf-8"))
 			f.write('\n')
-			f.write(cdep[item].encode("utf-8"))
+			f.write(cdep[item2])
 			f.write('\n')
 			f.write('\n')
 			f.write('\n')
 		f.write('\n')
 f.close()
+
