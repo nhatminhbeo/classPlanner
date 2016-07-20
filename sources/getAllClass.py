f3bd115ea215e1342851bd5a6299d48da3353bfa
Minh Tran <tqnmb437@gmail.com>
2016-07-20 02:48:04 +0000

    found all classes

---
+++ sources/getAllClass.py
@@ -0,0 +1,43 @@
+import lxml
+from lxml import html
+import requests
+import json
+import sys
+import re
+
+
+f = open('final_res', 'r+')
+
+with open('url') as urlfile:
+	for line in urlfile:
+		url = "http://ucsd.edu/catalog/courses/"+line
+		#url = "http://ucsd.edu/catalog/courses/CSE.html"
+
+		url = url.strip()
+		f.write(url)
+		page = requests.get(url)
+		tree = html.fromstring(page.content)
+		#subject = tree.xpath( "//h1[@class=\"noTopMargin\"]/text()")
+		#subject[0] = re.sub('[\t\n\r]', ' ', subject[0])
+		#print subject[0]
+		ids = tree.xpath( "//a[@id and @name]/@id" )
+		#print ids
+
+		cname = tree.xpath( "//p[@class=\"course-name\"]/text() | //p[@class=\"course-name\"]/descendant::*/text()" )
+		cdep = tree.xpath( "//p[@class=\"course-descriptions\"]/text() | //p[@class=\"course-descriptions\"]/descendant::*/text()" )
+
+		#print cname
+		#print cdep[:30]
+		final = []
+		cdep2 = []
+
+		#print cname
+		for item in range(len(cname)):
+			cname[item] = cname[item].strip(' \t\n\r ')
+			cname[item] = re.sub('[\t\n\r]', '', cname[item])
+			while "  " in cname[item]:
+				cname[item] = re.sub('  ', ' ', cname[item])
+			f.write (cname[item].encode("utf-8"))
+			f.write('\n')
+		f.write('\n')
+f.close()
